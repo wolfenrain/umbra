beebb632973cdaae350991313adcb3eccd701f11
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-05-04 18:57:17 +0200

    chore: created umbra using `very_good_cli`

---
+++ lib/main_staging.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:umbra/app/app.dart';
+import 'package:umbra/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
