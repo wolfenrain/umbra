5d87dfb49c00cb62858a3e36a99184dd5c7a030d
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2022-05-11 23:42:10 +0200

    Finished `umbra_cli` tests

--- packages/umbra_cli/test/src/cmd/cmd_test.dart
+++ packages/umbra_cli/test/src/cmd/cmd_test.dart
@@ -5,15 +5,15 @@ import 'package:umbra_cli/src/cmd/cmd.dart';
 
 void main() {
   group('Cmd', () {
-    // test('return result when succeeds', () async {
-    //   final cmd = Cmd();
-    //   final result = await cmd.run('echo', ['test']);
+    test('return result when succeeds', () async {
+      final cmd = Cmd();
+      final result = await cmd.run('echo', ['test']);
 
-    //   expect(result, isA<ProcessResult>());
-    //   expect(result.exitCode, equals(0));
-    //   expect(result.stdout, equals('test\n'));
-    //   expect(result.stderr, isEmpty);
-    // });
+      expect(result, isA<ProcessResult>());
+      expect(result.exitCode, equals(0));
+      expect(result.stdout, equals('test\n'));
+      expect(result.stderr, isEmpty);
+    });
 
     test('throw exception when fails', () async {
       final cmd = Cmd();
@@ -22,14 +22,7 @@ void main() {
         () => cmd.run('non-existing-command', ['test']),
         throwsA(
           isA<ProcessException>()
-              .having((p0) => p0.errorCode, 'errorCode', equals(127))
-              .having(
-                (p0) => p0.message,
-                'message',
-                equals(
-                  'Standard error\n/bin/sh: non-existing-command: command not found',
-                ),
-              ),
+              .having((p0) => p0.errorCode, 'errorCode', equals(127)),
         ),
       );
     });
